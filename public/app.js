2f5bf234b487cbc354d31e9db56f0552bce70a53
Sara Cashman <saramcashman@gmail.com>
2017-01-18 08:48:49 -0500

    add checklist

--- public/app.js
+++ public/app.js
@@ -1,96 +1,217 @@
 $(document).ready(function() {
-   
 
 
-    $.getJSON("http://localhost:8080/employees", function(data) {
+
+            $.getJSON("http://localhost:8080/employees", function(data) {
 
 
-        $.each(data, function(index, value) {
+                        $.each(data, function(index, value) {
 
 
-            $(".employeeList").append('<div class="employeeBox"><p><span class="firstName">' + value.first + ' </span><span class="lastName">' + value.last + ' </span><br><span class="employeeEmail">' + value.email + ' </span><br><span class="employeePhone">' + value.phone + ' </span><br><span class="hireddate">' + value.hired + ' </span><span class="startdate">' + value.start + ' </span><br><i class="fa fa-pencil-square-o" aria-hidden="true"></i><i class="fa fa-trash-o" aria-hidden="true"></i></p></div>')
+                            $(".employeeList").append('<div class="employeeBox"><p class=' + value._id + '><span class="firstName">' + value.first + ' </span><span class="lastName">' + value.last + ' </span><br><span class="employeeEmail">' + value.email + ' </span><br><span class="employeePhone">' + value.phone + ' </span><br><span class="hireddate">' + value.hired + ' </span><br><span class="startdate">' + value.start + ' </span><br><i class="fa fa-envelope-o" aria-hidden="true"></i><i class="fa fa-pencil-square-o editButton" aria-hidden="true" id="editButton"></i><i class="fa fa-trash-o deleteButton" aria-hidden="true" id="' + value._id + '"></i><i class="fa fa-check-square showChecklist" id="'+value._id+'" aria-hidden="true"></i><button class="updateButton">Update</button></p></div>')
 
 
-        });
+                        });
 
-    });
 
-    // full calendar
+                        $('.employeeList').on('click', 'div p i.showChecklist', function(e) {
 
-    // function makeEventsDraggable() { 
-    //     $( ".fc-draggable" ).draggable({
-    //         zIndex: 999,
-    //         revert: true,      // will cause the event to go back to its
-    //         revertDuration: false  //  original position after the drag
-    //     });
-    // }
-   var calender = $('#calendar').fullCalendar({
-        header: {
-                left: 'prev,next today',
-                center: 'title',
-                right: 'month,agendaWeek,agendaDay'
-            },
-            editable: true,
-            droppable: true, // this allows things to be dropped onto the calendar
-            dragRevertDuration: 0,
-            defaultView: 'agendaWeek',
-            selectable: true,
-				selectHelper: true,
-				/*
-					when user select timeslot this option code will execute.
-					It has three arguments. Start,end and allDay.
-					Start means starting time of event.
-					End means ending time of event.
-					allDay means if events is for entire day or not.
-				*/
-				select: function(start, end, allDay)
-				{
-					/*
-						after selection user will be promted for enter title for event.
-					*/
-					var title = prompt('Event Title:');
-					/*
-						if title is enterd calendar will add title and event into fullCalendar.
-					*/
-					if (title)
-					{
-						calendar.fullCalendar('renderEvent',
-							{
-								title: title,
-								start: start,
-								end: end,
-								allDay: allDay
-							},
-							true // make the event "stick"
-							
-						);
-						$(this).draggable({
-                			zIndex: 999,
-                			revert: true,      // will cause the event to go back to its
-                			revertDuration: 0  //  original position after the drag
-            });
-					}
-				}
-            // drop: function() {
-            //     makeEventsDraggable();
-            //     // is the "remove after drop" checkbox checked?
-            //     if ($('#drop-remove').is(':checked')) {
-            //         // if so, remove the element from the "Draggable Events" list
-            //         $(this).remove();
-            //     }
-            // },
-            // eventDragStop: function( event, jsEvent, ui, view ) {
-            //     makeEventsDraggable();
-            // },
-            // eventResize: function( event, delta, revertFunc, jsEvent, ui, view ) {
-            //     makeEventsDraggable();
-            // },
-            // viewRender: function() {
-            //     makeEventsDraggable();
-            // },
-            // eventDragStart:function( event, jsEvent, ui, view ) {
-            //     dragged = [ calendar, event ];
-            // },
-        });
-  
+                            $.each(data, function(index, value) {
+
+
+                             $(".traininglist").append('<div class="checklistBox"><p class=' + value._id + '><span class="I9"> I9 Complete?' + value.I9 + ' </span><br><span class="W4"> W4 Complete?' + value.W4 + ' </span><br><span class="International"> Are they an international student? '+value.International+'</span><br><span class="PayOption"> Which pay option did they choose? '+value.PayOption+'</span><br><span class="Register">Have they been register trained?'+value.Register+'</span><br><span class="RegisterDate">Training Date'+value.RegisterDate+'</span><br><span class="Refunds"> Have they been trained on returns?'+value.Refunds+'</span><br><span class="ReturnsDate">Training Date'+value.ReturnsDate+'</span><br><span class="CustServ">Have they been trained on customer service?'+value.CustServ+'</span><br><span class="CSDate">Training Date '+value.CSDate+'</span><br><span class="GM">Have they been trained on general merchandise? '+value.GM+'</span><br><span class="GMDate">Training Date'+value.GMDate+'</span><br><span class="TextDepart"> Have they been trained in textbooks?'+value.TextDepart+'</span><br><span class="TXDate">Training Date'+value.TXDate+'</span></p></div>')
+
+                                            });
+                                  });
+
+
+
+
+                        // $('#deleteButton').click(function() {
+                        $('.employeeList').on('click', 'div p i.deleteButton', function(e) {
+                            console.log('OW')
+
+                            var buttonId = $(this).attr('id');
+                            console.log(buttonId)
+                            $.ajax({
+                                url: "http://localhost:8080/employees/" + buttonId,
+                                type: "Delete",
+                                success: function() {
+                                    console.log($('i#' + buttonId).parent('p').parent('div'));
+                                    $('i#' + buttonId).closest('div').remove();
+
+                                    console.log("item deleted");
+
+                                }
+                            })
+
+                        });
+                        $('.employeeList').on('click', 'div p i#editButton', function(e) {
+                                    $(this).parent("p").children('.updateButton').show();
+
+                                    var first = $(this).parent("p").children("span.firstName").text();
+                                    $(this).parent("p").children('span.firstName').html("<input id='editName' name='editName' type='text' value='" + first + "'>")
+                                    var last = $(this).parent("p").children("span.lastName").text();
+                                    $(this).parent("p").children('span.lastName').html("<input id='editLastName' name='editLastName' type='text' value='" + last + "'>")
+                                    var email = $(this).parent("p").children("span.employeeEmail").text();
+                                    $(this).parent("p").children('span.employeeEmail').html("<input id='editEmail' name='editEmail' type='text' value='" + email + "'>")
+                                    var phone = $(this).parent("p").children("span.employeePhone").text();
+                                    $(this).parent("p").children('span.employeePhone').html("<input id='editPhone' name='editPhone' type='text' value='" + phone + "'>")
+                                    var hired = $(this).parent("p").children("span.hireddate").text();
+                                    $(this).parent("p").children('span.hireddate').html("<input id='editHireDate' name='editHireDate' type='text' value='" + hired + "'>")
+                                    var start = $(this).parent("p").children("span.startdate").text();
+                                    $(this).parent("p").children('span.startdate').html("<input id='editStartDate' name='editStartDate' type='text' value='" + start + "'>")
+
+
+                                    $('.updateButton').click(function() {
+
+                                        var buttonClassUpdate = $(this).parent('p').attr('class')
+                                        var editedFirst = $('p.' + buttonClassUpdate).children('span').children('input#editName').val();
+                                        var editedLast = $('p.' + buttonClassUpdate).children('span').children('input#editLastName').val();
+                                        var editedEmail = $('p.' + buttonClassUpdate).children('span').children('input#editEmail').val();
+                                        var editedPhone = $('p.' + buttonClassUpdate).children('span').children('input#editPhone').val();
+                                        var editedHired = $('p.' + buttonClassUpdate).children('span').children('input#editHireDate').val();
+                                        var editedStart = $('p.' + buttonClassUpdate).children('span').children('input#editStartDate').val();
+
+
+
+                                        var updateObject = new Object();
+                                        updateObject._id = buttonClassUpdate;
+                                        updateObject.first = editedFirst;
+                                        updateObject.last = editedLast;
+                                        updateObject.email = editedEmail;
+                                        updateObject.phone = editedPhone;
+                                        updateObject.hired = editedHired;
+                                        updateObject.start = editedStart;
+                                        console.log(updateObject);
+
+                                        $.ajax({
+                                            url: "http://localhost:8080/employees/" + buttonClassUpdate,
+                                            type: "PUT",
+                                            data: updateObject,
+                                            success: function() {
+
+                                                console.log(buttonClassUpdate);
+
+                                            }
+
+
+                                        });
+                                    });
+
+								});
+						
+							  
+
+					
+
+                                    $('.formtoggle').click(function() {
+                                        $('.formWrapper').toggle();
+                                        $('.trainingChecklist').toggle();
+                                    });
+
+                                    
+
+
+                                    // // full calendar
+
+                                    // function makeEventsDraggable() {
+                                    //     $(".fc-draggable").draggable({
+                                    //         zIndex: 999,
+                                    //         revert: true, // will cause the event to go back to its
+                                    //         revertDuration: false //  original position after the drag
+                                    //     });
+                                    // }
+
+                                    //     var dragged = null;
+
+                                    //     /* initialize the external events
+                                    //     -----------------------------------------------------------------*/
+
+                                    //     $('#external-events .fc-event').each(function() {
+
+                                    //         // store data so the calendar knows to render an event upon drop
+                                    //         $(this).data('event', {
+                                    //             title: $.trim($(this).text()), // use the element's text as the event title
+                                    //             stick: true // maintain when user navigates (see docs on the renderEvent method)
+                                    //         });
+
+                                    //         // make the event draggable using jQuery UI
+                                    //         $(this).draggable({
+                                    //             zIndex: 999,
+                                    //             revert: true, // will cause the event to go back to its
+                                    //             revertDuration: 0 //  original position after the drag
+                                    //         });
+
+                                    //     });
+
+
+                                    //     /* initialize the calendar
+                                    //     -----------------------------------------------------------------*/
+
+                                    //     var calendar = $('#calendar').fullCalendar({
+                                    //         googleCalendarApiKey: 'AIzaSyAuQJYARMDJHguyenuq9Uj03xEKrJIMFHI',
+                                    //         events: {
+                                    //             googleCalendarId: '8v3ebnkk7iq003gkfdag6g6hmo@group.calendar.google.com'
+                                    //         },
+                                    //         header: {
+                                    //             left: 'prev,next today',
+                                    //             center: 'title',
+                                    //             right: 'month,agendaWeek,agendaDay'
+                                    //         },
+                                    //         editable: true,
+                                    //         droppable: true, // this allows things to be dropped onto the calendar
+                                    //         dragRevertDuration: 0,
+                                    //         drop: function() {
+                                    //             makeEventsDraggable();
+                                    //             // is the "remove after drop" checkbox checked?
+                                    //             if ($('#drop-remove').is(':checked')) {
+                                    //                 // if so, remove the element from the "Draggable Events" list
+                                    //                 $(this).remove();
+                                    //             }
+                                    //         },
+                                    //         eventDragStop: function(event, jsEvent, ui, view) {
+                                    //             makeEventsDraggable();
+                                    //         },
+                                    //         eventResize: function(event, delta, revertFunc, jsEvent, ui, view) {
+                                    //             makeEventsDraggable();
+                                    //         },
+                                    //         viewRender: function() {
+                                    //             makeEventsDraggable();
+                                    //         },
+                                    //         eventDragStart: function(event, jsEvent, ui, view) {
+                                    //             dragged = [calendar, event];
+                                    //         },
+                                    //     });
+
+
+                                    //     /* Make external-events droppable
+                                    //     -----------------------------------------------------------------*/
+                                    //     $('#external-events-listing').droppable({
+                                    //         drop: function(event, ui) {
+                                    //             if (dragged) {
+                                    //                 var event = dragged[1];
+                                    //                 dragged[0].fullCalendar('removeEvents', event._id);
+                                    //                 var el = $("<div class='fc-event'>").appendTo(this).text(event.title);
+                                    //                 el.draggable({
+                                    //                     zIndex: 999,
+                                    //                     revert: true,
+                                    //                     revertDuration: 0
+                                    //                 });
+                                    //                 el.data('event', {
+                                    //                     title: event.title,
+                                    //                     id: event.id,
+                                    //                     stick: true
+                                    //                 });
+                                    //                 dragged = null;
+                                    //                 makeEventsDraggable();
+                                    //             }
+                                    //         }
+                                    //     });
+
 });
+
+
+
+
+  });
--- server.js
+++ server.js
@@ -6,6 +6,8 @@ var app = express();
 var router = express.Router();
 var db;
 
+var nodemailer = require('nodemailer');
+
 var Schema = mongoose.Schema;
 
 var NewEmployee = new Schema({
@@ -13,23 +15,25 @@ var NewEmployee = new Schema({
     last: String,
     email: String,
     phone: String,
-    hired: String,
     start: String,
-});
-
-var TrainingList = new Schema({
-    I9: Boolean,
-    W4: Boolean,
-    International: Boolean,
+    I9: String,
+    W4: String,
+    International: String,
     Payment: String,
-    Register: Boolean,
-    Rentals: Boolean,
-    Refunds: Boolean,
-    CustServ: Boolean,
-    GM: Boolean,
-    TextDepart: Boolean,
-
+    Register: String,
+    RegisterDate: String,
+    Refunds: String,
+    ReturnsDate: String,
+    CustServ: String,
+    CSDate: String,
+    GM: String,
+    GMDate: String,
+    TextDepart: String,
+    TXDate: String
 });
+
+
+   
 
 var Employee = mongoose.model('Employee', NewEmployee);
 
@@ -50,8 +54,21 @@ app.post('/employees', function(req, res) {
         last: req.body.last,
         email: req.body.email,
         phone: req.body.phone,
-        hired: req.body.hired,
-        start: req.body.start
+        start: req.body.start,
+        I9: req.body.I9,
+        W4: req.body.W4,
+        International: req.body.International,
+        PayOption: req.body.PayOption,
+        Register: req.body.Register,
+        RegisterDate: req.body.RegisterDate,
+        Refunds: req.body.Refunds,
+        ReturnsDate: req.body.ReturnsDate,
+        CustServ: req.body.CustServ,
+        CSDate: req.body.CSDate,
+        GM: req.body.GM,
+        GMDate: req.body.GMDate,
+        TextDepart: req.body.TextDepart,
+        TXDate: req.body.TXDate
     });
 
     employee.save(function(err) {
@@ -83,8 +100,10 @@ app.get('/employees/:_id', function(req, res) {
 
 // delete a book
 app.delete('/employees/:_id', function(req, res) {
+  console.log(req.params._id)
     Employee.findByIdAndRemove(req.params._id, function(err, employee) {
         if (err) {
+          console.log("error message")
             return res.status(500).json({
                 // message: 'Internal Server Error'
             });
@@ -107,11 +126,23 @@ app.put('/employees/:_id', function(req, res) {
         last: req.body.last,
         email: req.body.email,
         phone: req.body.phone,
-        hired: req.body.hired,
-        start: req.body.start
+        start: req.body.start,
+        I9: req.body.I9,
+        W4: req.body.W4,
+        International: req.body.International,
+        PayOption: req.body.PayOption,
+        Register: req.body.Register,
+        RegisterDate: req.body.RegisterDate,
+        Refunds: req.body.Refunds,
+        ReturnsDate: req.body.ReturnsDate,
+        CustServ: req.body.CustServ,
+        CSDate: req.body.CSDate,
+        GM: req.body.GM,
+        GMDate: req.body.GMDate,
+        TextDepart: req.body.TextDepart,
+        TXDate: req.body.TXDate
     });
-    console.log(req.params)
-    console.log(req.body)
+    
     Employee.findOneAndUpdate(queryID, updateThis,
         function(err, employee) {
             if (err) {
@@ -126,9 +157,40 @@ app.put('/employees/:_id', function(req, res) {
 
 })
 
+
+function handleEmployeeEmail(req, res) {
+    
+    var transporter = nodemailer.createTransport({
+        service: 'Gmail',
+        auth: {
+            user: 'frontendmanager656@gmail.com', // Your email id
+            pass: 'Terrier35' // Your password
+        }
+    });
+
+    var text = 'Hello world from \n\n' + req.body.name;
+
+    var mailOptions = {
+    from: 'frontendmanager656@gmail.com', // sender address
+    to: 'saramcashman@gmail.com', // list of receivers
+    subject: 'FIRED', // Subject line
+    text: text //, // plaintext body
+    // html: '<b>Hello world ✔</b>' // You can choose to send an HTML body instead
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+    if(error){
+        console.log(error);
+        res.json({yo: 'error'});
+    }else{
+        console.log('Message sent: ' + info.response);
+        res.json({yo: info.response});
+    };
+});
+
 // var Checklist = mongoose.model('Checklist', TrainingList);
 
-// mongoose.connect('mongodb://Admin1:Password1@ds111178.mlab.com:11178/bookshelf')
+// mongoose.createConnection('mongodb://Admin1:Password1@ds111178.mlab.com:11178/bookshelf')
 
 // app.use(express.static('public'));
 
@@ -144,15 +206,19 @@ app.put('/employees/:_id', function(req, res) {
 //         I9: req.body.I9,
 //         W4: req.body.W4,
 //         International: req.body.International,
-//         Payment: req.body.Payment,
+//         PayOption: req.body.PayOption,
 //         Register: req.body.Register,
-//         Rentals: req.body.Rentals,
+//         RegisterDate: req.body.RegisterDate,
 //         Refunds: req.body.Refunds,
+//         ReturnsDate: req.body.ReturnsDate,
 //         CustServ: req.body.CustServ,
+//         CSDate: req.body.CSDate,
 //         GM: req.body.GM,
+//         GMDate: req.body.GMDate,
 //         TextDepart: req.body.TextDepart,
+//         TXDate: req.body.TXDate
 //     });
-
+//  console.log(list);
 //     list.save(function(err) {
 //         if (err)
 //             res.send(err);
@@ -170,7 +236,7 @@ app.put('/employees/:_id', function(req, res) {
 //     });
 // });
 
-// // get ONE Employee
+// // // get ONE Employee
 
 // app.get('/traininglist/:_id', function(req, res) {
 //     Checklist.findById(req.params._id, function(err, list) {
@@ -229,6 +295,130 @@ app.put('/employees/:_id', function(req, res) {
 
 // })
 
+// var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+// var passport = require('passport');
+// var gcal     = require('google-calendar');
+  
+//   var google_calendar = new gcal.GoogleCalendar(accessToken);
+
+// passport.use(new GoogleStrategy({
+//     clientID: "258527515068-ml668r91ifenl6uurhtegjrp4qsc7jc5.apps.googleusercontent.com",
+//     clientSecret: "cssIibY35vsRt0Lm13yVfh3p",
+//     callbackURL: "http://localhost:8082/auth/callback",
+//     scope: ['openid', 'email', 'https://www.googleapis.com/auth/calendar'] 
+//   },
+//   function(accessToken, refreshToken, profile, done) {
+
+//     //google_calendar = new gcal.GoogleCalendar(accessToken);
+
+//     return done(null, profile);
+//   }
+// ));
+
+// var util = require('util');
+// var express  = require('express');
+
+// var config = require('./config');
+// var gcal = require('../GoogleCalendar');
+
+// /*
+//   ===========================================================================
+//             Setup express + passportjs server for authentication
+//   ===========================================================================
+// */
+
+// var app = express();
+// var passport = require('passport')
+// var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+
+// app.configure(function() {
+//   app.use(express.cookieParser());
+//   app.use(express.bodyParser());
+//   app.use(express.session({ secret: 'keyboard cat' }));
+//   app.use(passport.initialize());
+// });
+// app.listen(8082);
+
+// passport.use(new GoogleStrategy({
+//     clientID: config.consumer_key,
+//     clientSecret: config.consumer_secret,
+//     callbackURL: "http://localhost:8082/auth/callback",
+//     scope: ['openid', 'email', 'https://www.googleapis.com/auth/calendar'] 
+//   },
+//   function(accessToken, refreshToken, profile, done) {
+//     profile.accessToken = accessToken;
+//     return done(null, profile);
+//   }
+// ));
+
+// app.get('/auth',
+//   passport.authenticate('google', { session: false }));
+
+// app.get('/auth/callback', 
+//   passport.authenticate('google', { session: false, failureRedirect: '/login' }),
+//   function(req, res) { 
+//     req.session.access_token = req.user.accessToken;
+//     res.redirect('/');
+//   });
+
+
+
+//   ===========================================================================
+//                                Google Calendar
+//   ===========================================================================
+
+
+// app.all('/', function(req, res){
+  
+//   if(!req.session.access_token) return res.redirect('/auth');
+  
+//   //Create an instance from accessToken
+//   var accessToken = req.session.access_token;
+
+//   gcal(accessToken).calendarList.list(function(err, data) {
+//     if(err) return res.send(500,err);
+//     return res.send(data);
+//   });
+// });
+
+// app.all('/:calendarId', function(req, res){
+  
+//   if(!req.session.access_token) return res.redirect('/auth');
+  
+//   //Create an instance from accessToken
+//   var accessToken     = req.session.access_token;
+//   var calendarId      = req.params.calendarId;
+  
+//   gcal(accessToken).events.list(calendarId, {maxResults:1}, function(err, data) {
+//     if(err) return res.send(500,err);
+    
+//     console.log(data)
+//     if(data.nextPageToken){
+//       gcal(accessToken).events.list(calendarId, {maxResults:1, pageToken:data.nextPageToken}, function(err, data) {
+//         console.log(data.items)
+//       })
+//     }
+    
+    
+//     return res.send(data);
+//   });
+// });
+
+
+// app.all('/:calendarId/:eventId', function(req, res){
+  
+//   if(!req.session.access_token) return res.redirect('/auth');
+  
+//   //Create an instance from accessToken
+//   var accessToken     = req.session.access_token;
+//   var calendarId      = req.params.calendarId;
+//   var eventId         = req.params.eventId;
+  
+//   gcal(accessToken).events.get(calendarId, eventId, function(err, data) {
+//     if(err) return res.send(500,err);
+//     return res.send(data);
+//   });
+// });
 
 exports.app = app;
 
